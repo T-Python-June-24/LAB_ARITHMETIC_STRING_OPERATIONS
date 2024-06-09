889e46c98e653422add25fe41a6c84a943fc63a4
Ghofran <ghofranalsanosi@gmail.com>
2024-06-09 13:45:26 +0300

    solve bonus

---
+++ bonus LAB_ARITHMETI.py
@@ -0,0 +1,11 @@
+phrase="My name is ghofran alsanosi , I'm excited to learn more"
+TWord="My"
+print(len(phrase))
+print(phrase.index(TWord))
+print(phrase.count(phrase))
+print(phrase.upper())
+print(phrase.lower())
+
+replace = phrase.replace("My", "Hi my")
+print(replace)
+print( phrase[-1])
